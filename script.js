e7f6af73c081f1398daa99983da1c25c23061af0
nyitrai100 <nyitrai100@gmail.com>
2023-05-13 15:44:43 +0100

    links

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-//you have to upload this code on live server, wont work on local host!
-//contact form starts
-
-// const form = document.querySelector("form"),
-// statusTxt = form.querySelector(".button-area span");
-
-
-// form.onsubmit = (e) =>{
-//     e.preventDefault();
-//     statusTxt.style.color = "#0D6EFD";
-//     statusTxt.style.display = "block";
-
-//     let xhr = new XMLHttpRequest(); //creating new xml object
-//     xhr.open("POST", "message.php", true);
-//     xhr.onload = () =>{
-//         if(xhr.readyState == 4 && xhr.status == 200){
-//             let response = xhr.response;
-//             if(response.indexOf("Email and password field is required") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1 ){
-//                 statusTxt.style.color = "red";
-//             } else{
-//                 form.reset();
-//                 setTimeout(()=>{
-//                     statusTxt.style.display = "none";
-//                 }, 3000);
-//             }
-//             statusTxt.innerText = response;
-//         }
-//     }
-
-//     let formData = new FormData(form);
-
-//     xhr.send(formData);
-// }
-
-//contact form ends
 
 // the header JS
 
@@ -44,9 +9,8 @@ window.addEventListener("load", function(event) {
     document.body.classList.add('imgloaded');
 });
 
+
 //send email
-
-
 function sendEmail(){
   
     Email.send({
@@ -68,7 +32,7 @@ function sendEmail(){
   
   };
 
-  //alert boxes
+  //alert box for the first project
   function myFunction() {
     alert("This website is not available");
   }
